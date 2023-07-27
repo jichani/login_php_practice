d132a68d6121147efd94a7c7d2d1025ea942f140
jichani <jichan7571@naver.com>
2023-07-27 15:19:32 +0900

    로그아웃 버튼도 구현 완료

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+// 세션을 시작하는 함수, 세션을 저장하거나 저장된 세션을 사용할 때는 먼저 세션을 시작해야 한다.
+session_start();
+// 세션 변수 검사
+if (isset($_SESSION["userid"])) $user_id = $_SESSION["userid"];
+else $user_id = "";
+
+if (isset($_SESSION["userlevel"])) $user_level = $_SESSION["userlevel"];
+else $user_level = "";
+?>
+
 <!DOCTYPE html>
 <html lang="ko">
 
@@ -14,9 +25,25 @@
       <a href="index.php">동성로랑 sns연동 회원가입 프로젝트</a>
     </h3>
     <ul id="top_menu">
-      <li><a href="member_form.php">회원가입</a></li>
-      <li>|</li>
-      <li><a href="login_form.php">로그인</a></li>
+      <!-- 신기한 php 문법. echo로도 가능하다. -->
+      <?php
+      // 로그인 상태가 아니라면 회원가입, 로그인 메뉴 출력
+      if (!$user_id) {
+      ?>
+        <li><a href="member_form.php">회원가입</a></li>
+        <li>|</li>
+        <li><a href="login_form.php">로그인</a></li>
+      <?php
+        // 로그인 상태라면 회원정보, 로그아웃 메뉴 출력
+      } else {
+        $logged = $user_id . "(" . $user_level . ")";
+      ?>
+        <li><?php $logged ?></li>
+        <li>|</li>
+        <li><a href="logout.php">로그아웃</a></li>
+      <?php
+      }
+      ?>
     </ul>
   </div>
   <div id="menu_bar">
